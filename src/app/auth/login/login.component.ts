e3a073f5e8c40947533eda47d87e47de42b437d1
Tasneem-Ahmed-Mafhmoud <tasneemamed1999@gmail.com>
2023-03-03 00:27:12 +0200

    role Service

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+// import { DeviceDetectorService } from 'ngx-device-detector';
 
-import { DeviceDetectorService } from 'ngx-device-detector';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { RoleService } from 'src/app/services/role.service';
 
 @Component({
   selector: 'app-login',
@@ -18,29 +20,23 @@ loginForm:FormGroup=new FormGroup({
 
 })
 
-constructor(private router:Router ,private authService:AuthService ,private deviceService: DeviceDetectorService){}
-
-
+constructor(private router:Router ,private authService:AuthService ,private roleService:RoleService){}
 
 
 submitRegisterForm(loginForm:FormGroup){
-  // console.log(loginForm)
-
   if(loginForm.valid){
-  this.router.navigate(['/home'])
   this.authService.login(this.loginForm.value).subscribe((response)=>{
-
-
-    // console.log(response);
-    if(response.message=="success"){
-      // this.authService.checkLogin.next(true);
-console.log(response.message)
-     localStorage.setItem('userToken',response.token);
+    console.log(response.data);
+    if(response.data != null){
+      this.roleService.getInfo(response.data.role,response.data);
+      console.log(this.roleService.instructor.getValue());
+     localStorage.setItem('userToken',response.data.token);
      this.authService.token();
      this.router.navigate(['/home'])
     }else{
 
-this.error=response.errors.email.message
+this.error=response.message
+
     }
 
  }
--- src/app/instructor/services/informations.service.ts
+++ src/app/instructor/services/informations.service.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 
 
@@ -11,5 +13,17 @@ import { Injectable } from '@angular/core';
 })
 export class InformationsService {
 
-  constructor() { }
+  constructor(private httpClient:HttpClient) { }
+
+
+
+
+getInstructorInfo(id:number):Observable<any>{
+  return  this.httpClient.get(`http://127.0.0.1:8000/api//instructor/${id}`)
+}
+
+
+
+
+
 }
